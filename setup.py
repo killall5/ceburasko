8ac75ae90eb278b0f10dc7a3035beadc073f6f90
Alexey Tamarevskiy <mirror@inetra.ru>
2015-09-03 12:03:27 +0600

    Bump version to 5.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def files(basedir, dirs):
 
 setup(
     name='ceburasko',
-    version='4.1.12',
+    version='5.0.0',
     description='Ceburasko Django app',
     long_description='Django application for store crashes',
     author='Alexey Tamarevskiy',
