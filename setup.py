ced0f81cc782c0688e616ab9e8f2eda58e1da0ef
Alexey Tamarevskiy <mirror@inetra.ru>
2015-09-10 17:54:31 +0600

    Fix type

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def files(basedir, dirs):
 
 setup(
     name='ceburasko',
-    version='5.1.0',
+    version='5.1.1',
     description='Ceburasko Django app',
     long_description='Django application for store crashes',
     author='Alexey Tamarevskiy',
