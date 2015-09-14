be3e87474bb3e5c15f39462e0ef44d70ddd5c708
Alexey Tamarevskiy <mirror@inetra.ru>
2015-09-14 13:37:20 +0600

    Upload coredumps when requested

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def files(basedir, dirs):
 
 setup(
     name='ceburasko',
-    version='5.1.2',
+    version='5.2.0',
     description='Ceburasko Django app',
     long_description='Django application for store crashes',
     author='Alexey Tamarevskiy',
