e8c62166b4e6ea51295e21f1ecc743091c9d67bf
Alexey Tamarevskiy <mirror@inetra.ru>
2015-09-15 18:47:36 +0600

    Remove coredump from accident (need refactor first)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def files(basedir, dirs):
 
 setup(
     name='ceburasko',
-    version='5.2.0',
+    version='5.2.1',
     description='Ceburasko Django app',
     long_description='Django application for store crashes',
     author='Alexey Tamarevskiy',
