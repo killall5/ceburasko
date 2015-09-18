34b37a76aad15cb662a514725ed868755ec9585e
Alexey Tamarevskiy <mirror@inetra.ru>
2015-09-18 13:53:26 +0600

    Fix valgrind_opt is not none

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def files(basedir, dirs):
 
 setup(
     name='ceburasko',
-    version='5.2.2',
+    version='5.2.3',
     description='Ceburasko Django app',
     long_description='Django application for store crashes',
     author='Alexey Tamarevskiy',
